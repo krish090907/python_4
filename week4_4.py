a15eda6fb43487e4fbeabc8432680e9397860ba9
krish090907 <24331a05d7@mvgrce.edu.in>
2026-03-29 17:44:20 +0530

    Add files via upload

---
+++ week4_4.py
@@ -0,0 +1,10 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+if a>b and a>c :
+    print(f"{a} is the maximum number")
+elif b>c :
+    print(f"{b} is the maximum number")
+else :
+    print(f"{c} is the maximum number")
+
